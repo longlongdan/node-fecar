16a735ac7b9e084fd47035dec0ac585116f1f222
longdan <112200@58.com>
2019-07-10 10:09:45 +0800

    [feat]:emmm

---
+++ bin/www.js
@@ -0,0 +1,21 @@
+const koa = require('koa');
+const app = new koa();
+const http = require('http');
+const mongoose = require('mongoose');
+
+//链接数据库
+mongoose.connect("mongodb://localhost:27017/test");
+const con = mongoose.connection;
+con.on('error',() => {
+    console.log("数据库链接失败");
+})
+con.once('open', () => {
+    console.log("数据库链接成功");
+})
+app.use(ctx => {
+    ctx.body = "Hello Index!"
+    //克隆gitlab上面的代码
+    console.log(123);
+})
+
+app.listen(3000);
